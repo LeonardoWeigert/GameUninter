4c7171041d899eff8ff6a0bcdd936f5dbb2c1d6e
Leonardo <llweigert@gmail.com>
2025-04-21 19:48:11 -0300

    [Add] create main loop menu

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-import pygame as pg
+from code.Game import Game
 
-
-print('Setup Started')
-pg.init()
-window = pg.display.set_mode(size = (600,480))
-print('Setup End')
-
-while True:
-    # Check for all events
-    for event in pg.event.get():
-        if  event.type == pg.QUIT:
-            pg.quit() # Close Windows
-            quit()
+game = Game()
+game.run()
